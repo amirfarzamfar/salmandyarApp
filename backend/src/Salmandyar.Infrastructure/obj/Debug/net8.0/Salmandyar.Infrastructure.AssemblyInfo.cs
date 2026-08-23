31f90fd32118b447fb9e6112d0981e7289ebfab2
amirreza <amirrezafarzamfar@gmail.com>
2026-08-24 01:00:50 +0330

    FixNpm

--- backend/src/Salmandyar.Infrastructure/obj/Debug/net8.0/Salmandyar.Infrastructure.AssemblyInfo.cs
+++ backend/src/Salmandyar.Infrastructure/obj/Debug/net8.0/Salmandyar.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Salmandyar.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5673afc7b65b4dba7ff701757af1be52a26c66b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25349d908f6fee7f4f6d8a3a3a026af9fc6ca495")]
 [assembly: System.Reflection.AssemblyProductAttribute("Salmandyar.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Salmandyar.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
